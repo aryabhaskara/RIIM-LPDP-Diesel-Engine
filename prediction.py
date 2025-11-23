b2a0f88cadf29b621d15f31d2d0b05d8e5d14b56
Arya Bhaskara Adiprabowo <157106561+aryabhaskara@users.noreply.github.com>
2025-11-23 10:37:28 +0000

    change model to json

--- prediction.py
+++ prediction.py
@@ -3,9 +3,9 @@ import xgboost as xgb
 def predict(data):
     #model1 = load_model('model/ANN_Torque_FINAL.h5')
     model1 = xgb.XGBRegressor()
-    model1.load_model('model/XGB_TQ.model')    
+    model1.load_model('model/XGB_TQ.json')    
     model2 = xgb.XGBRegressor()
-    model2.load_model('model/XGB_SFC_MAE_final.model')
+    model2.load_model('model/XGB_SFC.json')
     model3 = xgb.XGBRegressor()
-    model3.load_model('model/XGB_TE.model')
+    model3.load_model('model/XGB_TE.json')
     return model1.predict(data), model2.predict(data), model3.predict(data)
